4b0a2408b0ec97fb90c017cb35dbdcb16dc7a40a
vlada-vey <vlada.dushutina@gmail.com>
2025-05-26 23:10:00 +0300

    Create ble_simple_peripheral.py

---
+++ ble_simple_peripheral.py
@@ -0,0 +1,18 @@
+ble = bluetooth.BLE()
+    p = BLESimplePeripheral(ble)
+
+    def on_rx(v):
+        print("RX", v)
+
+    p.on_write(on_rx)
+
+    i = 0
+    while True:
+        if p.is_connected():
+            # Short burst of queued notifications.
+            for _ in range(3):
+                data = str(i) + "_"
+                print("TX", data)
+                p.send(data)
+                i += 1
+        time.sleep_ms(100)
